ca30dc4b5cc7bd94649f3ce7b70069ab8a1b7a0e
DmitriyYar <1120597@mail.ru>
2022-09-04 15:48:41 +0400

    add lesson-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
-import { useState } from 'react';
-// import style from './App.module.css';
-import { Message } from './components/Message/Message';
-import { Form } from './components/Form/Form';
+import { useState, useEffect } from 'react';
+import { Form } from './components/Form';
+import { MessageList } from './components/MessageList';
+import { AUTHOR } from 'src/constants';
 
 export const App = () => {
-  const [messageList, setMessageList] = useState([]);
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = (newMessage) => {
+    setMessages((prevMessage) => [...prevMessage, newMessage]);
+    console.log(messages)
+  }
+
+  useEffect(() => {
+    if (messages.length > 0 &&
+      messages[messages.length - 1].author === AUTHOR.user
+    ) {
+      const timeout = setTimeout(() => {
+        addMessage({
+          author: AUTHOR.bot,
+          text: 'Im Bot'
+        });
+      }, 1500)
+
+      return () => clearTimeout(timeout);
+    }
+  }, [messages])
 
   return (
     <div className="App">
-      <Form arr={messageList} hendelChangeMessageList={setMessageList} />
-      <Message data={messageList.length} />
-      <div>
-        <p>
-          <u>messageList:</u>
-        </p>
-        {messageList.map((message, index) => (
-          <div key={index}>
-            {' '}
-            {message.author}: {message.text}
-          </div>
-        ))}
-      </div>
+      <MessageList messages={messages} />
+      <Form addMessage={addMessage} />
     </div>
   );
 };
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,89 +1,27 @@
-import style from './Form.module.css';
-import { useState, useEffect } from 'react';
+import { useState } from "react";
+import style from "./Form.module.css";
+// import { AUTHOR } from "src/constants";
 
-export const Form = ({ arr, hendelChangeMessageList }) => {
-  const [author, setAuthor] = useState('');
+export const Form = ({ addMessage }) => {
+  const [author, setAuthor] = useState("");
   const [text, setText] = useState('');
-  const [isTyping, setIsTyping] = useState(true);
-  const [quantity, setQuantity] = useState(null);
 
-  const handleClick = () => {
-    if (author !== '' && text !== '') {
-      setIsTyping(false);
-
-      hendelChangeMessageList([
-        ...arr,
-        {
-          text: text,
-          author: author,
-        },
-      ]);
-    } else {
-      console.log('Заполните поля');
-    }
-  };
-
-  const authorChange = (event) => {
-    setIsTyping(true);
-    setAuthor(event.target.value);
-  };
-
-  const textChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const handleClickInfo = () => {
-    let user = arr.find((element) => element.author === author);
-    let counter = 0;
-    if (user) {
-      arr.forEach((element) => {
-        if (author === element.author) {
-          counter = counter + 1;
-        }
-      });
-    }
-    setQuantity(counter);
-  };
-
-  useEffect(() => {
-    // пропускаем событие во время набора имени автора
-    if (isTyping) {
-      return;
-    }
-    setTimeout(() => {
-      console.log('Статья опубликована. Автор:  ' + author);
-    }, 1500);
-  }, [author, isTyping]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addMessage({
+      author: 'USER',//AUTHOR.user, 
+      text,
+    });
+    setText('');
+  }
 
   return (
-    <div className={style.card}>
-      <h3 className={style.title}>App component (parent)</h3>
-      <p className={style.title}>Автор:</p>
-      <input
-        className={style.field}
-        type="text"
-        onChange={authorChange}
-        placeholder="Автор"
-      ></input>
-      <p className={style.title}>Текст:</p>
-      <input
-        className={style.field}
-        type="text"
-        onChange={textChange}
-        placeholder="Статья"
-      ></input>
-      <button className={style.btn} onClick={handleClick}>
-        Send
-      </button>
-      <div className={style.info}>
-        <p>Автор: {author}</p>
-        <p className={style.infoBox}>
-          Опубликовал статей: {quantity}{' '}
-          <button className={style.btn} onClick={handleClickInfo}>
-            Info
-          </button>
-        </p>
-      </div>
-    </div>
+    <form className={style.card} onSubmit={handleSubmit}>
+      <h3 className={style.title}>Форма отправки</h3>
+      <input className={style.field} type="text" value={author} onChange={(event) => setAuthor(event.target.value)} placeholder="автор" disabled></input>
+      <input className={style.field} type="text" value={text} onChange={(event) => setText(event.target.value)} placeholder="текст"></input>
+      <button className={style.btn} disabled={!text}>Send</button>
+      {/* <Button label="send" disabled={!text} /> */}
+    </form>
   );
 };
