f53d0eef25970c66253ddb128943aa619ff25bd8
DmitriyYar <1120597@mail.ru>
2022-09-11 23:14:47 +0400

    add lesson-5

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,16 @@ export interface Message {
   text: string;
 }
 
-export type Messages = Message[];
+// export type Messages = Message[];
+
+// 1 вариант
+// export type Messages = {
+//   [key: string]: Message[];
+// };
+//2 вариант - это объект: ключ- срока, значения - массив данных
+export type Messages = Record<string, Message[]>;
+
+export interface Chat {
+  id: string;
+  name: string;
+}
